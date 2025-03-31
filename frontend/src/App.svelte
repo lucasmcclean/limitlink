9ea1ca3098ef4bd112073a176dc45b893a55c11f
Lucas McClean <lucksdeveloper@gmail.com>
2025-03-31 19:53:09 -0400

    feat(ui): add exo2 font

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,3 +1,3 @@
-<main>
-  <h1>URL Shortener</h1>
+<main class="h-screen flex items-center justify-center">
+  <h1 class="text-6xl font-display font-extrabold tracking-widest">URL Shortener</h1>
 </main>
--- handler/views.go
+++ handler/views.go
@@ -4,10 +4,8 @@ import "net/http"
 
 func ServeViews(mux *http.ServeMux) {
 	staticFS := http.FileServer(http.Dir("./static"))
-	assetsFS := http.FileServer(http.Dir("static/assets"))
 
 	mux.Handle("/static/", http.StripPrefix("/static/", staticFS))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", assetsFS))
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/index.html")
