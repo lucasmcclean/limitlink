6d6438dcdaec7a5eb963082020d952295006e785
Lucas McClean <lucksdeveloper@gmail.com>
2025-03-30 22:09:47 -0400

    feat: add eslint, update frontend dependencies

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess';
 
 export default {
-  preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    sveltePreprocess({
+      typescript: {
+        transpileOnly: true,
+      },
+    })
+  ],
 }
