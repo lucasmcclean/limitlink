6d38a286b9a12cc08a21ac69f5fbf8f6032bfe4e
Lucas McClean <lucksdeveloper@gmail.com>
2025-03-31 22:32:16 -0400

    feat(ui): add url input

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,3 +1,10 @@
-<main class="h-screen flex items-center justify-center">
+<main class="h-screen flex flex-col items-center justify-center gap-8">
   <h1 class="text-6xl font-display font-extrabold tracking-widest">URL Shortener</h1>
+  <form class="">
+    <input
+      class="w-md px-3 py-1 text-xl border rounded-lg"
+      placeholder="www.example.com"
+      type="text"
+    />
+  </form>
 </main>
