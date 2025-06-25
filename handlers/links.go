07d10a25a65bc2e974609273b4bb667487d09c62
Lucas McClean <lucksdeveloper@gmail.com>
2025-06-25 10:42:54 +0200

    feat(ui): more user friendly time display

--- handlers/links.go
+++ handlers/links.go
@@ -37,7 +37,7 @@ func Links(repo link.Repository) http.HandlerFunc {
 		}
 
 		w.WriteHeader(http.StatusCreated)
-		if err := tmpl.ExecuteTemplate(w, "new-link", lnk); err != nil {
+		if err := tmpl.ExecuteTemplate(w, "new-link", lnk.IntoDisplay()); err != nil {
 			log.Printf("template execution error: %v", err)
 			http.Error(w, "There was an error loading the page. Please try again.", http.StatusInternalServerError)
 		}
