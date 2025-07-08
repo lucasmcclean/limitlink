cdc2ad2b9fb510cde5840f5e4db9dd8684d48029
Lucas McClean <lucksdeveloper@gmail.com>
2025-07-08 10:39:32 +0200

    feat(back): add post, patch endpoints for link

--- backend/server/routes.go
+++ backend/server/routes.go
@@ -3,8 +3,10 @@ package server
 import (
 	"net/http"
 
+	"github.com/lucasmcclean/limitlink/handler"
 	"github.com/lucasmcclean/limitlink/link"
 )
 
 func registerRoutes(mux *http.ServeMux, repo link.Repository) {
+	mux.Handle("/link", handler.Link(repo))
 }
